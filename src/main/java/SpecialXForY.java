264bd5271c7d2a7e6aadecf1650af206fcbece06
jessica <Jess@Jessicas-MacBook-Pro.local>
2018-09-13 15:10:48 -0400

    finishing up X for y special

--- src/main/java/SpecialXForY.java
+++ src/main/java/SpecialXForY.java
@@ -1,12 +1,13 @@
 public class SpecialXForY extends Special {
+
     private int mNumberThatMustBePurchased;
     private double mPriceForSet;
+
     public SpecialXForY(int numberThatMustBePurchased, double priceForSet) {
         super(numberThatMustBePurchased);
         mNumberThatMustBePurchased = numberThatMustBePurchased;
         mPriceForSet = priceForSet;
     }
-
     public int getNumberThatMustBePurchased(){
         return mNumberThatMustBePurchased;
     }
